7939cff3acd75c7a0508caa22d92c043d4dde627
Naha1981 <138687467+Naha1981@users.noreply.github.com>
2026-09-20 09:47:34 +0200

    feat(jev): add isolated browser integration package

---
+++ app/integrations/jev/__init__.py
@@ -0,0 +1,4 @@
+from .client import JevBrowserAgent, JevBrowserError
+from .models import JevMission, JevMissionResult
+
+__all__ = ["JevBrowserAgent", "JevBrowserError", "JevMission", "JevMissionResult"]
